96097b6568d22a76c23001681ba60938f3d59578
chananelz <chananelz@gmail.com>
2022-12-21 22:02:48 +0200

    error message work

--- FSWD-2/scripts/login_signup.js
+++ FSWD-2/scripts/login_signup.js
@@ -1,9 +1,15 @@
-let singInBtn = document.getElementById("singinbtn");
-let singUpBtn = document.getElementById("singupbtn");
-let mailField = document.getElementById("mail-field");
-let title = document.getElementById("title");
-let forgetPassword = document.getElementById("forget-password")
-let secondPassword = document.getElementById("second-password")
+const btn = document.getElementById("continue-btn");
+const singInBtn = document.getElementById("singinbtn");
+const singUpBtn = document.getElementById("singupbtn");
+const title = document.getElementById("title");
+const forgetPassword = document.getElementById("forget-password")
+
+const username = document.getElementById("username");
+const mailField = document.getElementById("mail-field");
+const password = document.getElementById("password");
+const secondPassword = document.getElementById("second-password")
+
+// ------------------------------------------------
 
 singInBtn.onclick =function(){
   mailField.style.maxHeight = "0";
@@ -26,4 +32,62 @@ singUpBtn.onclick =function(){
   singInBtn.classList.add("disable");
   forgetPassword.style.display ="none";
 
+}
+// -------------------------------------------------
+
+btn.addEventListener("click",(e) => {
+  // The preventDefault() method cancels the event if it is cancelable, meaning that the default 
+  // action that belongs to the event will not occur.
+  e.preventDefault()
+  check_inputs();
+});
+
+function check_inputs() {
+  const usernameValue = username.querySelector("input").value.trim();
+	const emailValue = mailField.querySelector("input").value.trim();
+	const passwordValue = password.querySelector("input").value.trim();
+	const password2Value = secondPassword.querySelector("input").value.trim();
+  
+  if(usernameValue === '') {
+		setErrorFor(username, 'קלט ריק');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(mailField, 'קלט ריק');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(mailField, 'קלט לא חוקי');
+	} else {
+		setSuccessFor(mailField);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'קלט ריק');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(secondPassword, 'קלט ריק');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(secondPassword, "חוסר התאמה");
+	} else{
+		setSuccessFor(secondPassword);
+	}
+}
+
+
+function setErrorFor(input, message) {
+	const small = input.querySelector('small');
+	input.className = 'input-field error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	input.className = 'input-field success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
